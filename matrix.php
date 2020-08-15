c2232f439377d17a32768984e1c67f0a0397819e
Mari Leipola <mari.leipola@gmail.com>
2020-08-15 12:55:14 +0300

    It works

--- matrix.php
+++ matrix.php
@@ -1,68 +1,56 @@
-$string_start = array("1","0","1","<>","0","1","0","<>","1","0","-1");
-//var_dump($string_start); 
+<?php 
 
+function MatrixChallenge($strArr) {
 
-//callback function for array_filter, removes <>
+ //filter <>, new array consists only of numbers
+$filtered = array_filter($strArr, function($var) {
+  return (is_numeric($var)); 
+});
 
-function numeric($var){
-  
-    return (is_numeric($var));
+//Let's count how many <> there was. +1 is because we are chunking next
+//and we need one more chunk than we had <>
+$diff_count = count($strArr) - count($filtered) + 1;
 
-  
-}
-
- //new array without <> 
-$filtered = array_filter ($string_start, "numeric");
-
-//print_r($filtered); die;
-
-//Let's count how many <> there was. +1 is because we are chunking next and the count tells how many chunks
-//there is going to be
-$diff_count = count($string_start) - count($filtered) + 1;
-
+//array of numbers is divided to equal chunks
 $chunked = array_chunk($filtered, $diff_count);
 
 //Every sub-array is same size, so we can compare the size of array
 // (rows) to first sub-arrays size (columns)
-
 if(sizeof($chunked) != sizeof($chunked[0])){
   echo "not possible";
-}
-
-// Returns true if mat[N][N] is  
-// symmetric, else false 
-
-function isSymmetric($mat, $rows) 
-{ 
-    $tr = array(array()); 
-    for ($i = 0; $i < $rows; $i++) {
-        for ($j = 0; $j < $rows; $j++) {
-            $tr[$i][$j] = $mat[$j][$i]; 
-        }
-    }
-    // Fills transpose of  
-    // mat[N][N] in tr[N][N]  
-    for ($i = 0; $i < $rows; $i++) {
-        for ($j = 0; $j < $rows; $j++){ 
-            if ($mat[$i][$j] != $tr[$i][$j]){ 
-                return false; 
-            }
-    return true; 
-        }
-}
+  die;
 } 
-  
-    // Driver code 
-    $mat= $chunked;
-                  
-    if (isSymmetric($mat, sizeof($chunked))) {
-        echo "symmetric"; 
-    } else {
-        echo "not symmetric"; 
+ 
+// Echo symmetric if array(=$chunked)is  
+// symmetric, else not symmetric
+
+    //transpose is done with this block
+    $tr = array(array()); 
+    for ($i = 0; $i < sizeof($chunked); $i++) {
+        for ($j = 0; $j < sizeof($chunked); $j++) {
+            $tr[$i][$j] = $chunked[$j][$i]; 
+            
+        }
     }
-  
-  
+    // Comparing original array with transposed 
+    
+    for ($i = 0; $i < sizeof($chunked); $i++) {
+        for ($j = 0; $j < sizeof($chunked); $j++){ 
+          
+            if ($chunked[$i][$j] != $tr[$i][$j]){ 
+              echo 'not symmetric'; 
+              die; 
+            }
+        }  
+    }
+    echo 'symmetric';  
 
-  //print_r($chunked); die;
+} 
+    
+//array("1","2","1","<>","2","1","2","<>", "1","2","1")
 
+   
+// keep this function call here  
+ echo MatrixChallenge(fgets(fopen('php://stdin', 'r')));  
 
+?>
